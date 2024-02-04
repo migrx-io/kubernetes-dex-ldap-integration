dbd6042beb3a114ce53ae69ae72464d110cf3535
Anatolii Makarov <anatolii.makaroff@gmail.com>
2024-02-04 11:07:59 -0500

    disable proxy

--- setup.sh
+++ setup.sh
@@ -104,8 +104,7 @@ docker run -d --restart always \
 # install dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
-kubectl proxy
-
+# kubectl proxy
 # open dashboard in browser
 # http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 #
